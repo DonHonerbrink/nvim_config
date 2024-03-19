7ce0523ee4703a5076d0d4caedbfc2ceb5dffa3e
Don Honerbrink <dhonerbrink@gmail.com>
2024-03-19 07:07:35 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# vim_config
+My personal vim configuration
+
+To use, clone the repo to ~/.config/nvim:
+
+	git clone https://github.com/DonHonerbrink/nvim.git ~/.config/nvim
+
+
---
+++ init.lua
@@ -0,0 +1,88 @@
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Packer manages itself
+  use 'neovim/nvim-lspconfig'  -- Add lspconfig
+  use 'nvim-tree/nvim-tree.lua'
+  use 'kyazdani42/nvim-web-devicons'
+end)
+
+
+require'lspconfig'.clangd.setup{}
+require'nvim-tree'.setup {}
+require'nvim-web-devicons'.setup {}
+
+-- vim.cmd("colorscheme sierra")
+vim.cmd("colorscheme one")
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.tabstop = 4
+vim.o.wrap = false
+
+colorIndex = 1
+
+function GetAllColors()
+    local colors = {}
+
+    -- Check each potential colors directory in the runtime path
+    for _, source in ipairs(vim.api.nvim_list_runtime_paths()) do
+        local colors_dir = source .. '/colors'
+        -- Check if directory exists before reading
+        local exists = vim.fn.isdirectory(colors_dir)
+        if exists ~= 0 then -- if the directory exists, isdirectory returns 1, otherwise 0
+            local ok, files = pcall(vim.fn.readdir, colors_dir)
+            if ok then -- Only proceed if readdir was successful
+                for _, f in ipairs(files) do
+                    if f:match('%.vim$') then
+                        local cname = f:sub(1, #f - 4)
+                        table.insert(colors, cname)
+                    end
+                end
+            end
+        end
+    end
+
+    -- Remove duplicate entries
+    local unique_colors = {}
+    for _, v in ipairs(colors) do
+        unique_colors[v] = true
+    end
+
+    -- Convert back to list
+    local final_colors = {}
+    for k, _ in pairs(unique_colors) do
+        table.insert(final_colors, k)
+    end
+
+    return final_colors
+end
+
+function NextColors()
+    local allColors = GetAllColors()
+    colorIndex = colorIndex + 1
+    if colorIndex > #allColors then
+        colorIndex = 1
+    end
+
+    vim.cmd('colorscheme ' .. allColors[colorIndex])
+    print('[' .. colorIndex .. '/' .. #allColors .. ']:' .. allColors[colorIndex])
+end
+
+function PrevColors()
+    local allColors = GetAllColors()
+
+    colorIndex = colorIndex - 1
+    if colorIndex <= 0 then
+        colorIndex = #allColors 
+    end
+
+    vim.cmd('colorscheme ' .. allColors[colorIndex])
+    print('[' .. colorIndex .. '/' .. #allColors .. ']:' .. allColors[colorIndex])
+end
+
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>b', ':echo system(findfile("build.sh", ";"))<CR>', { noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<leader>d', ':echo system(findfile("buildrun.sh", ";"))<CR>', { noremap = true, silent = false})
+vim.api.nvim_set_keymap('i', '<C-n>', '<C-x><C-o>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-G>', ':lua PrevColors()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-H>', ':lua NextColors()<CR>', {noremap = true, silent = true})
+
